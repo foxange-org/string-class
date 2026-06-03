01d17442da3f49b4cfbf344a6f9d940813257a9e
foxange-org <slc871205abc@outlook.com>
2026-06-03 20:47:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -395,3 +395,5 @@ app.post('/api/logout', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
